7c91e78bb845dcdcf3f215f8ed82ab12c423e814
DavorMagno <DavorMagno@gmail.com>
2019-08-30 23:08:51 +0100

    Empezando a programr en Python

---
+++ python_basics/python_basics/helloWorld.py
@@ -0,0 +1,14 @@
+# Comment: This is a fairly simple Python script
+
+print('Hello, World!')
+#& E:/ANACONDA/python.exe -m pip install -U pylin
+#a = 300
+#b = 600
+#if a is b:
+ #   print('a es b')
+#else:
+ #   print('a no es b')
+#for i in range (10):
+ #   print(i)
+for i in range(5, 20, 3):
+    print(i)
